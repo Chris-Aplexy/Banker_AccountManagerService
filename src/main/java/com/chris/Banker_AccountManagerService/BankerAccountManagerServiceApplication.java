e047581e89b040fd1bdc1a7ad81c4f983a2cdfbe
Chris-Aplexy <mcaplexyc@gmail.com>
2022-10-10 14:32:25 +0300

    initial commit

---
+++ src/main/java/com/chris/Banker_AccountManagerService/BankerAccountManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_AccountManagerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankerAccountManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankerAccountManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chris/Banker_AccountManagerService/BankerAccountManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.Banker_AccountManagerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankerAccountManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
